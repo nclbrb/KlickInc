f5a3a20c9f2e5bec0161b53c50dff71a0f4e4a3a
Nicole Borabo <borabonicole19@gmail.com>
2025-04-27 11:32:24 +0800

    creation of table for budget

--- KlickInc/database/migrations/2025_04_27_013457_add_budget_fields_to_projects_table.php
+++ KlickInc/database/migrations/2025_04_27_013457_add_budget_fields_to_projects_table.php
@@ -4,25 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddBudgetFieldsToProjectsTable extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->decimal('budget', 15, 2)->nullable()->after('status');
+            $table->decimal('actual_expenditure', 15, 2)->default(0)->after('budget');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn('budget');
+            $table->dropColumn('actual_expenditure');
         });
     }
-};
+}
