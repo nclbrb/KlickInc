4a9275bb1d42122e55dd629cb3de21f4d28eea8f
bella20-sch <bellamourine20@gmail.com>
2025-04-27 19:33:00 +0800

    added route for calculating the total amount used

--- KlickInc/routes/api.php
+++ KlickInc/routes/api.php
@@ -19,44 +19,24 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
-    // Get all users (filtered by role)
-    Route::get('/users', [UserController::class, 'index']);
-
     // User Endpoints
     Route::get('/users', [UserController::class, 'index']);
 
     // Project Endpoints:
-    // GET /api/projects - List projects (for project managers, this returns all projects;
-    // for team members, the controller logic can filter to return only their assigned projects)
     Route::get('/projects', [ProjectController::class, 'index']);
-
-    // POST /api/projects - Create a new project
     Route::post('/projects', [ProjectController::class, 'store']);
-
-    // GET /api/projects/{id} - Retrieve a single project by its ID
     Route::get('/projects/{id}', [ProjectController::class, 'show']);
-
-    // PUT /api/projects/{id} - Update an existing project
     Route::put('/projects/{id}', [ProjectController::class, 'update']);
-
-    // DELETE /api/projects/{id} - Delete a project
     Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
+
+    // ADD THIS LINE for project totals
+    Route::get('/projects/{projectId}/totals', [ProjectController::class, 'projectTotals']);
 
     // Task Endpoints:
     Route::get('/tasks', [TaskController::class, 'index']);
-
-    // POST /api/tasks - Create a new task
     Route::post('/tasks', [TaskController::class, 'store']);
-
-    // GET /api/tasks/{id} - Retrieve a single task by its ID
     Route::get('/tasks/{id}', [TaskController::class, 'show']);
-
-    // PUT /api/tasks/{id} - Update an existing task
     Route::put('/tasks/{id}', [TaskController::class, 'update']);
-
-    // POST /api/tasks/{id}/assign - Assign a task to a user
     Route::post('/tasks/{id}/assign', [TaskController::class, 'assignTask']);
-
-    // DELETE /api/tasks/{id} - Delete a task
     Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
 });
